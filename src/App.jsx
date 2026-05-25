c8eba8980df8007fb4158a917661199f15f29407
Samueelljr <samueldejesus705@gmail.com>
2026-05-25 11:03:03 -0300

    Config Routes + Navbar/Footer + css

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+// Routes
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Register from './pages/Auth/Register'
+import Login from './pages/Auth/Login'
+
+// components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id='footer'>
+        <p>ReactGram &copy; 2026</p>
+    </footer>
+  )
+}
+
+export default Footer
