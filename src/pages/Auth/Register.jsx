f36dcbeb1ea2fd713b0b24e8ecd9664ae3f01341
Samueelljr <samueldejesus705@gmail.com>
2026-06-19 12:51:09 -0300

    Implementação do Redux + Config de requisições + Service de Auth + Slice de Auth + Conexão de form ao Redux + Exibição de msgs para o usuario

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,8 +1,86 @@
 import './Auth.css'
 
+// components
+import { Link } from 'react-router-dom'
+import Message from '../../components/Message'
+
+// hooks
+import { useState, useEffect } from 'react'
+
+// Redux
+import { register, reset } from '../../slices/authSlice'
+import { useSelector, useDispatch } from 'react-redux'
+
+
 const Register = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const dispatch = useDispatch()
+
+  const { loading, error } = useSelector((state) => state.auth)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user)
+
+    dispatch(register(user))
+  }
+
+  // Clean all auth states
+  useEffect(() => {
+
+    dispatch(reset())
+
+  }, [dispatch])
+
   return (
-    <div>Register</div>
+    <div id='register'>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='Nome'
+          onChange={(e) => setName(e.target.value)}
+          value={name || ''}
+        />
+        <input
+          type="email"
+          placeholder='Email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ''}
+        />
+        <input
+          type="password"
+          placeholder='Senha'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ''}
+        />
+        <input
+          type="password"
+          placeholder='Confirme sua senha'
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword || ''}
+        />
+        {!loading && <input type="submit" value='Cadastrar'/>}
+        {loading && <input type="submit" value='Aguarde...' disabled/>}
+        {error && <Message msg={error} type="error"/>}
+      </form>
+      <p>
+        Já tem conta? <Link to='/login'>Clique aqui.</Link>
+      </p>
+    </div>
   )
 }
 
