25282f07b653b7bba42f778e5f9c5149cf1d2bba
Samueelljr <samueldejesus705@gmail.com>
2026-07-28 10:56:31 -0300

    get photos + delete photos + add photos

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -14,41 +14,160 @@ import { useParams } from 'react-router-dom'
 
 // Redux
 import { getUserDetails } from '../../slices/userSlice'
+import { 
+    publishPhoto, 
+    resetMessage, 
+    getUserPhotos,
+    deletePhoto 
+} from '../../slices/photoSlice'
 
 const Profile = () => {
 
-    const {id} = useParams()
+    const { id } = useParams()
 
     const dispatch = useDispatch()
 
     const { user, loading } = useSelector((state) => state.user)
     const { user: userAuth } = useSelector((state) => state.auth)
+    const {
+        photos,
+        loading: loadingPhoto,
+        message: messagePhoto,
+        error: errorPhoto
+    } = useSelector((state) => state.photo)
 
-    // photo
-    
+    const [title, setTitle] = useState("")
+    const [image, setImage] = useState("")
+
+    // New form and edit form ref
+    const newPhotoForm = useRef()
+    const editPhotoForm = useRef()
+
 
     // load user data
     useEffect(() => {
         dispatch(getUserDetails(id))
+        dispatch(getUserPhotos(id))
     }, [dispatch, id])
 
-    if(loading) {
+    const resetComponentMessage = () => {
+        setTimeout(() => {
+            dispatch(resetMessage());
+        }, 2000);
+    }
+
+    const handleFile = (e) => {
+        const image = e.target.files[0]
+
+        setImage(image)
+
+    }
+
+
+    const submitHandle = (e) => {
+        e.preventDefault();
+
+        const photoData = {
+            title,
+            image,
+        };
+
+        console.log(title, image);
+
+        // Build FormData
+        const formData = new FormData();
+
+        Object.keys(photoData).forEach((key) => {
+            formData.append(key, photoData[key]);
+        });
+
+        dispatch(publishPhoto(formData));
+
+        setTitle("");
+
+        resetComponentMessage()
+    };
+
+    // Delete a photo
+    const handleDelete = (id) => {
+        
+        dispatch(deletePhoto(id))
+
+        resetComponentMessage()
+    }
+
+    if (loading) {
         return <p>Carregando...</p>
     }
 
-  return (
-    <div id='profile'>
-        <div className="profile-header">
-            {user.profileImage && (
-                <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+    return (
+        <div id='profile'>
+            <div className="profile-header">
+                {user.profileImage && (
+                    <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+                )}
+                <div className="profile-description">
+                    <h2>{user.name}</h2>
+                    <p>{user.bio}</p>
+                </div>
+            </div>
+
+            {id === userAuth._id && (
+                <>
+                    <div className="new-photo" ref={newPhotoForm}>
+                        <h3>Compartilhe algum momento.</h3>
+                        <form onSubmit={submitHandle}>
+                            <label>
+                                <span>Título para foto:</span>
+                                <input type="text" placeholder='Insira um título.' onChange={(e) => setTitle(e.target.value)} value={title || ""} />
+                            </label>
+
+                            <label>
+                                <span>Imagem:</span>
+                                <input type="file" onChange={handleFile} />
+                            </label>
+
+                            {!loadingPhoto && <input type="submit" value="Postar" />}
+                            {loadingPhoto && <input type="submit" disabled value="Aguarde..." />}
+                        </form>
+                    </div>
+                    {errorPhoto && <Message msg={errorPhoto} type="error" />}
+                    {messagePhoto && <Message msg={messagePhoto} type="success" />}
+                </>
             )}
-            <div className="profile-description">
-                <h2>{user.name}</h2>
-                <p>{user.bio}</p>
+            <div className="user-photos">
+                <h2>Fotos Publicadas</h2>
+                <div className="photos-container">
+                    {photos &&
+                        photos.map((photo) => (
+                            <div className="photo" key={photo._id}>
+                                {photo.image && (
+                                    <img src={`${uploads}/photos/${photo.image}`} alt={photo.title}
+                                    />
+                                )}
+
+                                {id === userAuth._id ? (
+                                    <div className="actions">
+
+                                        <Link to={`/photos/${photo._id}`}>
+                                            <BsFillEyeFill />
+                                        </Link>
+                                        <BsPencilFill />
+                                        <BsXLg onClick={() => handleDelete(photo._id)} />
+                                    </div>
+                                ) : (
+
+                                    <Link className="btn" to={`/photos/${photo._id}`}>
+                                        Ver
+                                    </Link>
+                                )}
+                            </div>
+                        ))}
+                    {photos.length === 0 && <p>Ainda não há fotos publicadas</p>}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Profile
---
+++ src/slices/photoSlice.js
@@ -0,0 +1,121 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import photoService from "../services/photoService";
+
+const initialState = {
+    photos: [],
+    photo: {},
+    error: false,
+    success: false,
+    loading: false,
+    message: null
+}
+
+// Publish user photo
+export const publishPhoto = createAsyncThunk (
+    "photo/publish",
+    async (photo, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.publishPhoto(photo, token)
+
+        // Check for errors
+        if(data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
+// Get user photos
+export const getUserPhotos = createAsyncThunk (
+    "photo/userphotos",
+    async (id, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.getUserPhotos(id, token)
+
+        return data
+    }
+)
+
+// Delete a photo
+export const deletePhoto = createAsyncThunk(
+    "photo/delete",
+    async(id, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.deletePhoto(id, token)
+
+        // Check for errors
+        if(data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
+export const photoSlice = createSlice({
+    name: "photo/publish",
+    initialState,
+    reducers: {
+        resetMessage: (state) => {
+            state.message = null
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+      .addCase(publishPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(publishPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photo = action.payload;
+        state.photos.unshift(state.photo);
+        state.message = "Foto publicada com seucesso!"
+      })
+      .addCase(publishPhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = null;
+      })
+      .addCase(getUserPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getUserPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
+      })
+      .addCase(deletePhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(deletePhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        
+        state.photos = state.photos.filter((photo) => {
+            return photo._id !== action.payload.id
+        });
+        state.message = action.payload.message;
+      })
+      .addCase(deletePhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = null;
+      })
+    }
+})
+
+export const { resetMessage } = photoSlice.actions
+export default photoSlice.reducer
